9b44c9b337c690fd11eeb38bc51d90c4ced1f222
Diane Ujeneza <dianesis17@gmail.com>
2019-10-17 17:37:32 +0200

    API key

--- app/src/main/java/com/moringaschool/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/MainActivity.java
@@ -1,41 +1,21 @@
-package com.moringaschool.myrestaurants;
+package com.moringaschool.myrestaurants.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.moringaschool.myrestaurants.R;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-//    public static final String TAG = MainActivity.class.getSimpleName();
-//    private Button mFindRestaurantsButton;
-//    private EditText mLocationEditText;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
-//
-//        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                String location = mLocationEditText.getText().toString();
-//                Log.d(TAG, location);
-//                Intent intent = new Intent(MainActivity.this,RestaurantsActivity.class);
-//                Toast.makeText(MainActivity.this,"Hello World!",Toast.LENGTH_LONG).show();
-//                startActivity(intent);
-//            }
-//        });
-//    }
 
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @BindView(R.id.locationEditText) EditText mLocationEditText;
@@ -54,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(v == mFindRestaurantsButton) {
             String location = mLocationEditText.getText().toString();
             Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+
             intent.putExtra("location", location);
             startActivity(intent);
         }
---
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/RestaurantsActivity.java
@@ -0,0 +1,108 @@
+package com.moringaschool.myrestaurants.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.moringaschool.myrestaurants.MyRestaurantsArrayAdapter;
+import com.moringaschool.myrestaurants.R;
+import com.moringaschool.myrestaurants.adapters.RestaurantListAdapter;
+import com.moringaschool.myrestaurants.models.Business;
+import com.moringaschool.myrestaurants.models.Category;
+import com.moringaschool.myrestaurants.models.YelpBusinessesSearchResponse;
+import com.moringaschool.myrestaurants.network.YelpApi;
+import com.moringaschool.myrestaurants.network.YelpClient;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+
+    private static final String TAG = RestaurantsActivity.class.getSimpleName();
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    private RestaurantListAdapter mAdapter;
+
+    public List<Business> restaurants;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        Toast.makeText(this, ""+location, Toast.LENGTH_SHORT).show();
+
+        YelpApi client = YelpClient.getClient();
+
+        Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
+
+        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+            @Override
+            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+                hideProgressBar();
+                if (response.isSuccessful()) {
+                    restaurants = response.body().getBusinesses();
+                    mAdapter = new RestaurantListAdapter(RestaurantsActivity.this, restaurants);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager =
+                            new LinearLayoutManager(RestaurantsActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showRestaurants();
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+                hideProgressBar();
+                showFailureMessage();
+            }
+
+        });
+    }
+
+    private void showFailureMessage() {
+        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showUnsuccessfulMessage() {
+        mErrorTextView.setText("Something went wrong. Please try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showRestaurants() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+}
